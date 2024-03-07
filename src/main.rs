fc4f81124e52514830afd2bf8443da92575f6649
zahash <zahash.z@gmail.com>
2024-03-07 16:29:40 +0530

    triangle! yay!

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use wgpu::{
-    Color, CommandEncoderDescriptor, CompositeAlphaMode, Device, DeviceDescriptor, Features,
-    Instance, Limits, LoadOp, Operations, PresentMode, Queue, RenderPassColorAttachment,
-    RenderPassDescriptor, RequestAdapterOptions, StoreOp, Surface, SurfaceConfiguration,
-    SurfaceError, TextureUsages, TextureViewDescriptor,
+    include_wgsl, Color, CommandEncoderDescriptor, CompositeAlphaMode, Device, DeviceDescriptor,
+    Features, Instance, Limits, LoadOp, Operations, PipelineLayoutDescriptor, PresentMode, Queue,
+    RenderPassColorAttachment, RenderPassDescriptor, RequestAdapterOptions, StoreOp, Surface,
+    SurfaceConfiguration, SurfaceError, TextureUsages, TextureViewDescriptor,
 };
 use winit::{
     dpi::PhysicalSize,
@@ -17,6 +17,7 @@ pub struct State<'window> {
     pub device: Device,
     pub queue: Queue,
     pub config: SurfaceConfiguration,
+    pub render_pipeline: wgpu::RenderPipeline,
 }
 
 impl<'window> State<'window> {
@@ -71,11 +72,58 @@ impl<'window> State<'window> {
 
         surface.configure(&device, &config);
 
+        let shader = device.create_shader_module(include_wgsl!("shader.wgsl"));
+
+        let render_pipeline_layout = device.create_pipeline_layout(&PipelineLayoutDescriptor {
+            label: Some("Render Pipeline Layout"),
+            bind_group_layouts: &[],
+            push_constant_ranges: &[],
+        });
+
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("Render Pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &shader,
+                entry_point: "vs_main",
+                buffers: &[],
+            },
+            fragment: Some(wgpu::FragmentState {
+                module: &shader,
+                entry_point: "fs_main",
+                targets: &[Some(wgpu::ColorTargetState {
+                    format: config.format,
+                    blend: Some(wgpu::BlendState::REPLACE),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList, // 1.
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw, // 2.
+                cull_mode: Some(wgpu::Face::Back),
+                // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
+                polygon_mode: wgpu::PolygonMode::Fill,
+                // Requires Features::DEPTH_CLIP_CONTROL
+                unclipped_depth: false,
+                // Requires Features::CONSERVATIVE_RASTERIZATION
+                conservative: false,
+            },
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState {
+                count: 1,
+                mask: !0,
+                alpha_to_coverage_enabled: false,
+            },
+            multiview: None, // 5.
+        });
+
         Self {
             surface,
             device,
             queue,
             config,
+            render_pipeline,
         }
     }
 
@@ -90,7 +138,7 @@ impl<'window> State<'window> {
                 label: Some("Render Encoder"),
             });
 
-        encoder.begin_render_pass(&RenderPassDescriptor {
+        let mut render_pass = encoder.begin_render_pass(&RenderPassDescriptor {
             label: Some("Render Pass"),
             color_attachments: &[Some(RenderPassColorAttachment {
                 view: &view,
@@ -109,6 +157,11 @@ impl<'window> State<'window> {
             timestamp_writes: None,
             occlusion_query_set: None,
         });
+
+        render_pass.set_pipeline(&self.render_pipeline);
+        render_pass.draw(0..3, 0..1);
+
+        drop(render_pass);
 
         self.queue.submit([encoder.finish()]);
         output.present();
